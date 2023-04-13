18155e360a297a040ed7f614fda15ac091194cc7
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-04-13 15:13:02 +0530

    Added: Ability to analyse mood

---
+++ src/main/java/com/bridgelabz/moodAnalyser/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.moodAnalyser;
+
+public class MoodAnalyser {
+    public String moodAnalyser(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
